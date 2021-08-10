cd6230ba20918904c61fa3a5f42e905523f748ed
Vano Akhvlediani <78465382+13Wise@users.noreply.github.com>
2021-08-10 20:14:42 +0400

    Update script.sh

--- Task2/script.sh
+++ Task2/script.sh
@@ -4,6 +4,11 @@ sudo amazon-linux-extras install epel
 sudo amazon-linux-extras install nginx1
 sudo nginx
 OS=$(uname -a)
-sudo echo "<!DOCTYPE html> <html> <head> <title>Exadel!</title> <style> body { width: 35em; margin: 0 auto; font-family: Tahoma, Verdana, Arial, sans-serif; } </style> </head> <body> <h1> Hello World</h1> <p>Os Version</p> $OS </body> </html>" > /usr/share/nginx/html/index.html
+cat <<EOF > /usr/share/nginx/html/index.html
+<html>
+<h2 align=center> Hello World!</h2><br>
+<p align=center>$OS</p>
+</html>
+EOF
 
 
