d134f026d97da51fbe357aaec120ad30c4d9e390
Vano Akhvlediani <78465382+13Wise@users.noreply.github.com>
2021-08-12 19:06:04 +0400

    Update main.tf

--- Task3/Main Task/main.tf
+++ Task3/Main Task/main.tf
@@ -205,6 +205,13 @@ resource "aws_security_group" "ubuntusg" {
       cidr_blocks = ["10.0.0.0/16"]
     }
   }
+  
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "SG UBUNTU"
